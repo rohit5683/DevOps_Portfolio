31fa2edbe36438c68f397f4f2c6aee327e7d5030
rohit5683 <killerrohitrocks007@gmail.com>
2024-10-10 11:21:23 +0530

    rival1

--- src/Pages/Techstack.jsx
+++ src/Pages/Techstack.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Techstack.css";
 import { TechstackList } from "../Utiles/TechstackList";
 import RubberBand from "react-reveal/RubberBand";
-// import Fade from "react-reveal/Fade";
+import Fade from "react-reveal/Fade";
 
 const Techstack = () => {
   return (
