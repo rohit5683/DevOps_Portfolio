b9af022f2bb5e578fa0d2676117d5421695343b7
rohit5683 <killerrohitrocks007@gmail.com>
2024-10-10 12:15:11 +0530

    change done in about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,38 +1,30 @@
 import React from "react";
 import "./About.css";
 import Profile from "../../src/Assets/Images/Profile.png";
-import Zoom from "react-reveal/Zoom";
+import { useSpring, animated } from "@react-spring/web"; // Import react-spring
 
 const About = () => {
+  // Define spring animation for the whole component
+  const springStyle = useSpring({
+    from: { opacity: 0, transform: 'translateY(20px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    config: { duration: 600 }, // Adjust the duration as needed
+  });
+
   return (
-    <>
-      <Zoom bottom>
-        <div className="about" id="about">
-          <div className="row">
-            <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-image">
-              <img src={Profile} alt="profile pic" />
-            </div>
-            <div className="col-md-6 about-content">
-              <h1>About me</h1>
-              <p>
-                I am Rohit Vishwakarma recently I have completed my post
-                graduation in Master of Computer Application from Doranda
-                College. I have a deep intrested in Web Development in which I
-                have made 3 projects using HTML, CSS, Javascript, NodeJs and
-                also learning JavaScript Library like react js to build a web
-                application. I have completed 6 weeks online training in Reactjs
-                from Internshala in which I learn frontend development with
-                Restful API. In MCA with the help of YouTube I have done my
-                project in MERN Stack. So, I can say that I can be the best fit
-                for this job because I am always curious to learn and explore
-                new things. I love technical challenges that help me to grow and
-                This is all about me.
-              </p>
-            </div>
-          </div>
+    <animated.div style={springStyle} className="about" id="about">
+      <div className="row">
+        <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-image">
+          <img src={Profile} alt="profile pic" />
         </div>
-      </Zoom>
-    </>
+        <div className="col-md-6 about-content">
+          <h1>About me</h1>
+          <p>
+            I am Rohit Vishwakarma, and I recently completed my post-graduation in Master of Computer Application from Doranda College. I have a deep interest in Web Development, having made three projects using HTML, CSS, JavaScript, and Node.js. I am also learning JavaScript libraries like React.js to build web applications. I completed six weeks of online training in React.js from Internshala, where I learned frontend development with RESTful APIs. During my MCA, with the help of YouTube, I worked on my project in the MERN stack. I believe I can be the best fit for this job because I am always curious to learn and explore new things. I love technical challenges that help me grow, and this is all about me.
+          </p>
+        </div>
+      </div>
+    </animated.div>
   );
 };
 
